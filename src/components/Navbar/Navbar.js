b86d7fcae7f339fbbf76bd398e879c95012a0e94
Pericles001 <periclesadjovi@gmail.com>
2022-08-31 22:21:30 +0100

    Edit navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import {Breakpoint} from "react-socks";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export type NavbarProps = {}
-
-export default function Navbar({}: NavbarProps) {
-    return (<div className="Navbar"/>);
+export function Navbar() {
+    return (
+        <div>
+            <Breakpoint small down>
+                <nav className="navbar navbar-light bg-light text-center">
+                    <span className="navbar-brand mb-0 h1 mx-auto">Quizreo</span>
+                </nav>
+            </Breakpoint>
+            <Breakpoint medium>
+                <nav className="p-2 navbar navbar-light bg-light">
+                    <span className="navbar-brand mb-0 h1">Quizreo</span>
+                    <div className="mb-0 justify-content-between align-items-left d-flex flex-row">
+                        <ul  className="p-2 list-inline">
+                            <li className="list-inline-item">
+                                <a className="text-decoration-none text-dark" href="#">About</a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a className="text-decoration-none text-dark" href="#">Features</a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a className="text-decoration-none text-dark" href="#">Get Started</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </Breakpoint>
+            <Breakpoint large up>
+                <div>
+                    <h1>hum</h1>
+                </div>
+            </Breakpoint>
+        </div>
+    );
 }
+
+
