7517118cac1ab7306d2a4e0fcc47240d121c415f
Pericles001 <periclesadjovi@gmail.com>
2022-08-31 22:21:42 +0100

    Update app file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import logo from './logo.svg';
 import {BreakpointProvider} from "react-socks";
-import {About} from "./components";
+import {About, Feature, Footer, Navbar} from "./components";
 import './App.css';
 
 function App() {
     return (
         <BreakpointProvider>
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-               <About/>
-                </header>
+                <Navbar/>
+                <About/>
+                <Feature/>
+                <Footer/>
             </div>
         </BreakpointProvider>
     );
