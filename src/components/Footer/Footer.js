f7f098eeb429e50b15938bef7e26e4aeb8c754a8
Pericles001 <periclesadjovi@gmail.com>
2022-08-31 22:20:04 +0100

    Edit component and export

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import {Breakpoint} from "react-socks";
 
-export type FooterProps = {}
-
-export default function Footer({}: FooterProps) {
-    return (<div className="Footer"/>);
+export function Footer() {
+    return (
+        <div>
+            <Breakpoint medium>
+                <div>
+                    <h1>Footer</h1>
+                </div>
+            </Breakpoint>
+            <Breakpoint large up>
+                <div>
+                    <h1>Bon</h1>
+                </div>
+            </Breakpoint>
+        </div>
+    );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import {About} from "./About/About";
 import {Feature} from "./Feature/Feature";
+import {Footer} from "./Footer/Footer";
+import {Navbar} from "./Navbar/Navbar";
 
-export {About, Feature};
+export {About, Feature, Footer, Navbar};
