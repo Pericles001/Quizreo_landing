9018437c7833b07cb2cdc7ca5a511c0efca1802b
Pericles001 <periclesadjovi@gmail.com>
2022-08-31 19:17:36 +0100

    Edit files to update components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import {BreakpointProvider} from "react-socks";
+import {About} from "./components";
 import './App.css';
 
 function App() {
@@ -11,14 +12,7 @@ function App() {
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+               <About/>
                 </header>
             </div>
         </BreakpointProvider>
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import {Breakpoint} from "react-socks";
 
-export type AboutProps = {}
 
-export default function About({}: AboutProps) {
-    return (<div className="About"/>);
+export function About() {
+    return (
+        <div>
+            <Breakpoint medium>
+                <div>
+                    <h1>About</h1>
+                </div>
+            </Breakpoint>
+            <Breakpoint large up>
+                <div>
+                    <h1>Bonjour</h1>
+                </div>
+            </Breakpoint>
+        </div>
+
+    );
 }
+
