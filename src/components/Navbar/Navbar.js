c90ebce941bac35085c64ef0aadbe888f962dad8
Pericles001 <periclesadjovi@gmail.com>
2022-08-31 23:00:23 +0100

    Add border bottom to navigation bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,12 +6,12 @@ export function Navbar() {
     return (
         <div>
             <Breakpoint small down>
-                <nav className="navbar navbar-light bg-light text-center">
+                <nav className="navbar navbar-light bg-light text-center" style={{borderBottom: "black 2px solid"}}>
                     <span className="navbar-brand mb-0 h1 mx-auto">Quizreo</span>
                 </nav>
             </Breakpoint>
-            <Breakpoint medium>
-                <nav className="p-2 navbar navbar-light bg-light">
+            <Breakpoint medium up>
+                <nav className="p-2 navbar navbar-light bg-light" style={{borderBottom: "black 2px solid"}}>
                     <span className="navbar-brand mb-0 h1">Quizreo</span>
                     <div className="mb-0 justify-content-between align-items-left d-flex flex-row">
                         <ul  className="p-2 list-inline">
@@ -28,11 +28,7 @@ export function Navbar() {
                     </div>
                 </nav>
             </Breakpoint>
-            <Breakpoint large up>
-                <div>
-                    <h1>hum</h1>
-                </div>
-            </Breakpoint>
+
         </div>
     );
 }
