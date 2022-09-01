2f65262ba0fe273a33aa978fe4d39f86165698fa
Pericles001 <periclesadjovi@gmail.com>
2022-09-01 02:58:48 +0100

    Add target

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ import {About} from "./About/About";
 import {Feature} from "./Feature/Feature";
 import {Footer} from "./Footer/Footer";
 import {Navbar} from "./Navbar/Navbar";
-
-export {About, Feature, Footer, Navbar};
+import {Features} from "./Feature/Features";
+export {About, Feature, Features, Footer, Navbar};
