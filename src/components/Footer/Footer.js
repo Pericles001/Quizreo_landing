de28668af23faa434a41ca33156233da8611d596
Pericles001 <periclesadjovi@gmail.com>
2022-09-01 15:45:22 +0100

    Edit footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,16 +4,21 @@ import {Breakpoint} from "react-socks";
 export function Footer() {
     return (
         <div>
-            <Breakpoint medium>
-                <div>
-                    <h1>Footer</h1>
-                </div>
+            <Breakpoint small down>
+                <nav className="Navbar navbar text-center" style={{borderTop: "black 2px solid"}}>
+                    <span className="navbar-brand mb-0 h1 mx-auto">Made with &hearts; by <a
+                        className="text-decoration-none text-dark" href="https://github.com/Pericles001">Pericles001</a></span>
+                </nav>
+    <p className="text-center mx-auto text-dark Trade" style={{backgroundColor: "#CCC7C7"}}>2022 Quizreo @ LLC</p>
             </Breakpoint>
-            <Breakpoint large up>
-                <div>
-                    <h1>Bon</h1>
-                </div>
+            <Breakpoint medium up>
+                <nav className="Navbar navbar text-center" style={{borderTop: "black 2px solid"}}>
+                    <span className="navbar-brand mb-0 h1 mx-auto">Made with &hearts; by <a
+                        className="text-decoration-none text-dark" href="https://github.com/Pericles001">Pericles001</a></span>
+                </nav>
+                <p className="text-center mx-auto text-dark Trade" style={{backgroundColor: "#CCC7C7"}}>2022 Quizreo @ LLC</p>
             </Breakpoint>
+
         </div>
     );
 }
