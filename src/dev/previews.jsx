0e5224e90bbd37baeb0a15323438800de25bfec2
Pericles001 <periclesadjovi@gmail.com>
2022-09-01 02:58:15 +0100

    Preview components

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import {PaletteTree} from './palette';
 import App from "../App";
+import {About, Feature} from "../components";
+import {Features} from "../components/Feature/Features";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/App">
                 <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/About">
+                <About/>
+            </ComponentPreview>
+            <ComponentPreview path="/Feature">
+                <Feature/>
+            </ComponentPreview>
+            <ComponentPreview path="/Features">
+                <Features/>
             </ComponentPreview>
         </Previews>
     );
