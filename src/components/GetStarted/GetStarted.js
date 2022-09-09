10e1f8d223311a3003082a27da89d2da782cc3a0
Pericles001 <periclesadjovi@gmail.com>
2022-09-09 03:23:40 +0100

    Edit button to redirect on external website

--- src/components/GetStarted/GetStarted.js
+++ src/components/GetStarted/GetStarted.js
@@ -1,8 +1,12 @@
 
+
 export function GetStarted(){
+    const handleClick = () => {
+        window.location.href = 'https://quizreofrontend1.netlify.app/';
+    }
     return (
         <div className="mt-5 mb-5 d-grid gap-2 col-6 mx-auto" id="StartSec">
-            <button className="btn btn-secondary shadow mb-3 disabled" type="submit"><h4>Get Started</h4></button>
+            <button className="btn btn-secondary shadow mb-3" type="submit" onClick={handleClick}><h4>Get Started</h4></button>
         </div>
     )
 }
